e871f8d3208b04fd345f0c5d395220c9949e94ba
Roman Zakolenko <zakolenkoroman@gmail.com>
2016-03-06 02:40:52 +0200

    fixed analyze

--- src/main/java/logic/StatisticsAnalyzer.java
+++ src/main/java/logic/StatisticsAnalyzer.java
@@ -2,6 +2,8 @@ package logic;
 
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.KNearestNeighbors;
+import net.sf.javaml.classification.evaluation.EvaluateDataset;
+import net.sf.javaml.classification.evaluation.PerformanceMeasure;
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.DenseInstance;
@@ -12,6 +14,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class StatisticsAnalyzer {
     private StatisticsUpdater statisticsUpdater;
@@ -33,10 +36,10 @@ public class StatisticsAnalyzer {
         for (int i = 0; i < length; i++) {
             writer.write(userData[i]);
 
-                System.out.println(userData[i]);
+            System.out.println(userData[i]);
 
         }
-        int columnNumber = userData[0].length() - userData[0].replaceAll(",", "").length()-1;
+        int columnNumber = userData[0].length() - userData[0].replaceAll(",", "").length() - 1;
         System.out.println(columnNumber);
         classificationProcessor.learn(file, columnNumber);
     }
@@ -45,24 +48,5 @@ public class StatisticsAnalyzer {
         return classificationProcessor.classify(instance);
     }
 
-    public static void main(String[] args) throws IOException {
-
-        StatisticsAnalyzer analyzer = new StatisticsAnalyzer(new StatisticsUpdater(), new ClassificationProcessor(), "/home/vladislav/data.data");
-
-        analyzer.learn();
-//        Dataset data = FileHandler.loadDataset(new File("C:\\Users\\Администратор\\Desktop\\iris.data"), 4, ",");
-
-        for(int i = 0; i <= 1; i++){
-            for(int j = 1; j <= 50; j++){
-                for(int k = 1; k < 3; k++) {
-                    Object o = analyzer.classify(new DenseInstance(new double[]{i*100, j, k*100}));
-                    System.out.println(i + ", " + j + ", " + k + " = " + o);
-                }
-            }
-        }
-
-//        Classifier knn = new KNearestNeighbors(5);
-//        knn.buildClassifier(data);
-//        System.out.println(knn.classify(new DenseInstance(new double[]{2.1, 1.2, 6.2, 0.2})));
-    }
+    
 }
