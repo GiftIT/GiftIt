333974a481b6bcf5876d1977086e1e49b22caa26
Roman Zakolenko <zakolenkoroman@gmail.com>
2016-03-06 16:07:12 +0200

    pass shop url to js

--- src/main/java/controller/GiftController.java
+++ src/main/java/controller/GiftController.java
@@ -6,8 +6,12 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -27,17 +31,37 @@ public class GiftController {
                        @RequestParam(value = "age") double age,
                        @RequestParam(value = "country") double country) {
         double[] criteria = {sex, age, country};
-        String[] result = null;
+        List<String> mlData = null;
+        StringBuffer buffer = null;
         try {
-            result = statisticsUpdater.getCategories(criteria, 5);
+            mlData = Arrays.asList(statisticsUpdater.getCategories(criteria, 5));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("vocabulary.json"), "UTF-8"));
+            buffer = new StringBuffer();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+            reader.close();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("result", result);
-        return jsonObject.toString();
-
+        JSONArray result = new JSONArray();
+        JSONObject jsonObject = new JSONObject(buffer.toString());
+        JSONArray jsonArray = jsonObject.getJSONArray("categories");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject json = (JSONObject) jsonArray.get(i);
+            if (mlData.contains(json.get("product"))) {
+                jsonArray.put(json);
+            }
+        }
+        return result.toString();
     }
 
 
