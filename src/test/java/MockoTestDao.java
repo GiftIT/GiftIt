e18c2f311039b1c4aaaaab89a1d6fca1cdf03c36
Roman Zakolenko <zakolenkoroman@gmail.com>
2016-11-07 00:16:52 +0200

    Add import

--- src/test/java/MockoTestDao.java
+++ src/test/java/MockoTestDao.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
 public class MockoTestDao {
